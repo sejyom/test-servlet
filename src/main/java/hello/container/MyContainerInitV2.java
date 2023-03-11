a48686d740d1196743d0278909f1b3f3f9c69c74
sejyom <akxxkd01@gmail.com>
2023-03-11 20:42:53 +0900

    servlet container

--- src/main/java/hello/container/MyContainerInitV2.java
+++ src/main/java/hello/container/MyContainerInitV2.java
@@ -5,6 +5,7 @@ import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.HandlesTypes;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.Set;
 
 @HandlesTypes(AppInit.class)
@@ -15,5 +16,15 @@ public class MyContainerInitV2 implements ServletContainerInitializer {
         System.out.println("MyContainerInitV2 c = " + c);
         System.out.println("MyContainerInitV2 ctx = " + ctx);
 
+        //class hello.container.AppInitV1Servlet
+        for (Class<?> appInitClass : c) {
+            try {
+                //new AppInitV1Servlet()과 같은 코드
+                AppInit appInit= (AppInit) appInitClass.getDeclaredConstructor().newInstance();
+                appInit.onStartup(ctx);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
